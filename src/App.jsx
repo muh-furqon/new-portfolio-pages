f533f3746b29cb4affff8de481af2239e588d58e
muh-furqon <37769126+muh-furqon@users.noreply.github.com>
2025-09-02 12:06:16 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import ProjectsPopup from './components/ProjectsPopup';
 import Loader from './components/Loader';
 import HeroSection from './components/HeroSection';
